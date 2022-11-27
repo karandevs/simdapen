f0a08d422a0c243e48f565c33a366b19ae577b3b
karandevs <karandevsingh1902@gmail.com>
2022-11-27 16:45:36 +0700

    Adding PensiunController@update and Adding code at View Pensiun

--- app/Http/Controllers/PensiunController.php
+++ app/Http/Controllers/PensiunController.php
@@ -70,7 +70,149 @@ class PensiunController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = [];
+        $pegawai = Pegawai::findOrFail($id);
+
+        if ($request->berkas_skcpns != $pegawai->berkas_skcpns && $request->berkas_skcpns != "") {
+            $validator = $request->validate([
+                'berkas_skcpns' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_skcpns' => $request->berkas_skcpns
+            ]);
+        }
+
+        if ($request->berkas_skpns != $pegawai->berkas_skpns && $request->berkas_skpns != "") {
+            $validator = $request->validate([
+                'berkas_skpns' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_skpns' => $request->berkas_skpns
+            ]);
+        }
+
+        if ($request->berkas_skpktterakhir != $pegawai->berkas_skpktterakhir && $request->berkas_skpktterakhir != "") {
+            $validator = $request->validate([
+                'berkas_skpktterakhir' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_skpktterakhir' => $request->berkas_skpktterakhir
+            ]);
+        }
+
+        if ($request->berkas_skjabatan != $pegawai->berkas_skjabatan && $request->berkas_skjabatan != "") {
+            $validator = $request->validate([
+                'berkas_skjabatan' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_skjabatan' => $request->berkas_skjabatan
+            ]);
+        }
+
+        if ($request->berkas_kgbterakhir != $pegawai->berkas_kgbterakhir && $request->berkas_kgbterakhir != "") {
+            $validator = $request->validate([
+                'berkas_kgbterakhir' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_kgbterakhir' => $request->berkas_kgbterakhir
+            ]);
+        }
+
+        if ($request->berkas_karpeg != $pegawai->berkas_karpeg && $request->berkas_karpeg != "") {
+            $validator = $request->validate([
+                'berkas_karpeg' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_karpeg' => $request->berkas_karpeg
+            ]);
+        }
+
+        if ($request->berkas_drp != $pegawai->berkas_drp && $request->berkas_drp != "") {
+            $validator = $request->validate([
+                'berkas_drp' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_drp' => $request->berkas_drp
+            ]);
+        }
+
+        if ($request->berkas_skhukuman != $pegawai->berkas_skhukuman && $request->berkas_skhukuman != "") {
+            $validator = $request->validate([
+                'berkas_skhukuman' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_skhukuman' => $request->berkas_skhukuman
+            ]);
+        }
+
+        if ($request->berkas_dsk != $pegawai->berkas_dsk && $request->berkas_dsk != "") {
+            $validator = $request->validate([
+                'berkas_dsk' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_dsk' => $request->berkas_dsk
+            ]);
+        }
+
+        if ($request->berkas_suratnikah != $pegawai->berkas_suratnikah && $request->berkas_suratnikah != "") {
+            $validator = $request->validate([
+                'berkas_suratnikah' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_suratnikah' => $request->berkas_suratnikah
+            ]);
+        }
+
+        if ($request->berkas_akteanak != $pegawai->berkas_akteanak && $request->berkas_akteanak != "") {
+            $validator = $request->validate([
+                'berkas_akteanak' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_akteanak' => $request->berkas_akteanak
+            ]);
+        }
+
+        if ($request->berkas_kpt != $pegawai->berkas_kpt && $request->berkas_kpt != "") {
+            $validator = $request->validate([
+                'berkas_kpt' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_kpt' => $request->berkas_kpt
+            ]);
+        }
+
+        if ($request->berkas_dpcp != $pegawai->berkas_dpcp && $request->berkas_dpcp != "") {
+            $validator = $request->validate([
+                'berkas_dpcp' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_dpcp' => $request->berkas_dpcp
+            ]);
+        }
+
+        if ($request->berkas_ppk2thnterakhir != $pegawai->berkas_ppk2thnterakhir && $request->berkas_ppk2thnterakhir != "") {
+            $validator = $request->validate([
+                'berkas_ppk2thnterakhir' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_ppk2thnterakhir' => $request->ppk2thnterakhir
+            ]);
+        }
+
+        if ($request->berkas_pasphoto != $pegawai->berkas_pasphoto && $request->berkas_pasphoto != "") {
+            $validator = $request->validate([
+                'berkas_pasphoto' => ['tinyint', 'max:1'],
+            ]);
+            $pegawai->update([
+                'berkas_pasphoto' => $request->berkas_pasphoto
+            ]);
+        }
+
+        if ($pegawai) {
+            return redirect()->back()->with('success','Data berhasil diubah!');
+        } else {
+            return redirect()->back()->withErrors($request);
+        }
     }
 
     /**
--- resources/views/menu/pensiun.blade.php
+++ resources/views/menu/pensiun.blade.php
@@ -9,6 +9,7 @@
                     <th scope="col">NIP</th>
                     <th scope="col">Nama</th>
                     <th scope="col">Estimasi terdekat</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -33,6 +34,120 @@
                                 @php
                                     echo $diff->m . ' bulan ' . $diff->d . ' hari';
                                 @endphp
+                            </td>
+                            <td>
+                                <button class="btn btn-sm btn-danger">Cetak</button>
+                                <button type="button" class="btn btn-sm btn-primary" data-bs-toggle="modal"
+                                    data-bs-target="#listModal{{ $i }}">Berkas</button>
+                                <form action="{{ route('pensiun.update', $value->id) }}" method="POST">
+                                    <div class="modal fade" id="listModal{{ $i }}" tabindex="-1"
+                                        aria-labelledby="listModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="listModalLabel">Informasi Berkas Pegawai
+                                                    </h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <ol class="list-group list-group-numbered border-left-primary">
+                                                        <li class="list-group-item d-flex">
+                                                            <div class="d-flex flex-column flex-grow-1">
+                                                                <div class="fw-bold"><b>Surat Keterangan Calon PNS</b></div>
+                                                                <div class="d-flex flex-row justify-content-between">
+                                                                    <h6>@php
+                                                                        if ($value->berkas_skcpns) {
+                                                                            echo 'Sudah';
+                                                                        } else {
+                                                                            echo 'Belum';
+                                                                        }
+                                                                    @endphp</h6>
+                                                                    <input type="checkbox" id="berkas_skcpns"
+                                                                        name="berkas_skcpns"
+                                                                        @php if ($value->berkas_skcpns) {
+                                                                            echo 'checked value="true"';
+                                                                        } else {
+                                                                            echo 'value="false"';
+                                                                        } @endphp>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li class="list-group-item d-flex">
+                                                            <div class="d-flex flex-column flex-grow-1">
+                                                                <div class="fw-bold"><b>Surat Keterangan PNS</b></div>
+                                                                <div class="d-flex flex-row justify-content-between">
+                                                                    <h6>@php
+                                                                        if ($value->berkas_skpns) {
+                                                                            echo 'Sudah';
+                                                                        } else {
+                                                                            echo 'Belum';
+                                                                        }
+                                                                    @endphp</h6>
+                                                                    <input type="checkbox" id="berkas_skpns"
+                                                                        name="berkas_skpns"@php if ($value->berkas_skcpns) {
+                                                                            echo 'checked value="true"';
+                                                                        } else {
+                                                                            echo 'value="false"';
+                                                                        } @endphp>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li class="list-group-item d-flex">
+                                                            <div class="d-flex flex-column flex-grow-1">
+                                                                <div class="fw-bold"><b>Surat Keterangan Pangkat
+                                                                        Terakhir</b></div>
+                                                                <div class="d-flex flex-row justify-content-between">
+                                                                    <h6>@php
+                                                                        if ($value->berkas_skpktterakhir) {
+                                                                            echo 'Sudah';
+                                                                        } else {
+                                                                            echo 'Belum';
+                                                                        }
+                                                                    @endphp</h6>
+                                                                    <input type="checkbox" id="berkas_skpktterakhir"
+                                                                        name="berkas_skpangkatterakhir"
+                                                                        @php if ($value->berkas_skpktterakhir) {
+                                                                            echo 'checked value="true"';
+                                                                        } else {
+                                                                            echo 'value="false"';
+                                                                        } @endphp>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <li class="list-group-item d-flex">
+                                                            <div class="d-flex flex-column flex-grow-1">
+                                                                <div class="fw-bold"><b>Surat Keterangan Jabatan</b></div>
+                                                                <div class="d-flex flex-row justify-content-between">
+                                                                    <h6>@php
+                                                                        if ($value->berkas_skjabatan) {
+                                                                            echo 'Sudah';
+                                                                        } else {
+                                                                            echo 'Belum';
+                                                                        }
+                                                                    @endphp</h6>
+                                                                    <input type="checkbox" id="berkas_skjabatan"
+                                                                        name="berkas_skjabatan" @php if ($value->berkas_skjabatan) {
+                                                                            echo 'checked value="true"';
+                                                                        } else {
+                                                                            echo 'value="false"';
+                                                                        } @endphp>
+                                                                </div>
+                                                            </div>
+                                                        </li>
+                                                        <input type="hidden" name="id" value="@php echo $value->id; @endphp">
+                                                    </ol>
+                                                </div>
+                                                <div class="modal-footer">
+                                                    <button type="button" class="btn btn-secondary"
+                                                        data-bs-dismiss="modal">Tutup</button>
+                                                    <button type="submit" class="btn btn-primary"
+                                                        data-bs-dismiss="modal">Simpan</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
                             </td>
                         </tr>
                         @php
